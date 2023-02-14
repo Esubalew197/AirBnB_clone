a4072ecfdaddb80dbfb27f99ca50b3dddbfeff0c
Esubalew197 <esubalewmillion@gmail.com>
2023-02-14 22:59:31 +0300

    test_city.py

---
+++ tests/test_models/test_city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines unittests for models/city.py.
+Unittest classes:
+    TestCity_instantiation
+    TestCity_save
+    TestCity_to_dict
+"""
+import os
+import models
+import unittest
+from datetime import datetime
+from time import sleep
+from models.city import City
